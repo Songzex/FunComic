7dc644ae72f9dc27fb8fc78ae32be38e84770e4c
Songzex <2402265378@qq.com>
2025-10-23 17:47:01 +0800

    begin chat模块

---
+++ fun-comic-chat/src/main/java/com/scy/chat/ChatApplication.java
@@ -0,0 +1,15 @@
+package com.scy.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+}
